681851f41ca3d098f4bfcaa8dfc9667df34ca06a
Milson Fortunato Neto <milsonfn@yahoo.com>
2024-11-09 15:26:50 -0300

    changeMigrationsDeleteCategoryColumn

--- src/database/migrations/20240218135211-delete-category-column.js
+++ src/database/migrations/20240218135211-delete-category-column.js
@@ -7,11 +7,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Products', {
-      category: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-    })
+    await queryInterface.createColumn('Products', 'category', Sequelize.STRING)
   },
 }
